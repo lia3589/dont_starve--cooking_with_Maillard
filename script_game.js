d09b21a5997e8cadb5643e110c164c20a8d0611e
lia3589 <19271706221@163.com>
2024-04-07 23:22:22 +0800

    On branch main  Changes to be committed: 	modified:   cook.html 	modified:   creator.html 	modified:   game.html 	new file:   pic/Meatballs.png 	modified:   script_game.js 	modified:   style_game.css 	modified:   style_mainpage.css

--- script_game.js
+++ script_game.js
@@ -1,2 +1,48 @@
-var overlay=document.getElementById('overlay')
-var popup=document.getElementById('popup')
+var rightDiv = document.getElementById('right');
+let clickCount=0;
+document.addEventListener('click', function(event) {
+    if (event.target.closest('#right')) {
+        addNewMessage();
+    }
+});
+
+function addNewMessage() {
+    clickCount++;
+    let content=''
+    switch(clickCount) {
+        case 1:
+            content='1';
+            break;
+        case 2:
+            content='2';
+            break;
+        case 3:
+            content='3';
+            break;
+        case 4:
+            content='4';
+            break;
+        case 5:
+            content='5';
+            break;
+        default:
+            return;
+    }
+    var newMessage = document.createElement('div');
+    var head=document.createElement('img');
+    if (clickCount%2==0) {
+        newMessage.classList.add('message-we');
+        head.src='pic/威尔逊.png';
+    } else {
+        newMessage.classList.add('message-op');
+        head.src='pic/Meatballs.png';
+    }
+    var messageContent = document.createElement('span');
+    messageContent.textContent = content;
+
+    newMessage.appendChild(head);
+    newMessage.appendChild(messageContent);
+
+    rightDiv.appendChild(newMessage);
+    rightDiv.scrollTop = rightDiv.scrollHeight;
+}
